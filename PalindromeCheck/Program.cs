b5a280e12ddab5a173ce7cc1a24870ed424ccedb
trinhnguyen212 <tngu212@gmail.com>
2023-08-07 17:42:19 +1200

    first commit'

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to check if it is Palindrome or not: ");
+            string string_input = Console.ReadLine();
+            string rev;
+
+            char[] ch = string_input.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            bool b = string_input.Equals(rev, StringComparison.OrdinalIgnoreCase);
+
+            if (b)
+            {
+                Console.WriteLine("" + string_input + " is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("" + string_input + " is not a Palindrome!");
+            }
+
+            Console.Read();
+        }
+    }
+}
